5ce5fbffe4f94034bd8bc77e3786537b02eafe7d
Alan Moreno <amoreno@perceptivesensors.com>
2025-02-07 15:18:29 -0700

    sanity schemas; created all components;hero banner component in progress

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link'
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">
+          small text
+        </p>
+        <h3>
+          mid
+        </h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/product/ID">
+            <button type='button'>button text</button>
+          </Link>
+          <div className="desc">
+            <h5>  
+              Description
+            </h5>
+            <p>
+              Description
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
