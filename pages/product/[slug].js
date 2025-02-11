81b644ba63573863f8780c8bf33ca05dbed531b5
Alan Moreno <amoreno@perceptivesensors.com>
2025-02-11 12:13:42 -0700

    products-detail page; useContext

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineStar, AiFillStar } from 'react-icons/ai'
+import { Product } from '../../components';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+
+  const [index, setIndex] = useState(0)
 
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="" />
+            <img className="product-detail-image" src={urlFor(image && image[0])} alt="" />
           </div>
-          {/* <div className="small-images-container">
-            {image?.map((item, index) => (
-              <img src={urlFor(item)} alt="other products" className="" onMouseEnter="" />
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img src={urlFor(item)} alt="other products" 
+                className={i === index  ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)} />
             ))}
-          </div> */}
+          </div>
         </div>
 
-        <div className="product-details-desc">
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
@@ -43,6 +48,25 @@ const ProductDetails = ({ product, products }) => {
               <span className="num" onClick="">0</span>
               <span className="plus" onClick=""><AiOutlinePlus /></span>
             </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
