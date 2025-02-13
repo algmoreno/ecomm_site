c63e1e99f0e435d22d53f85faeda3dcdf0317167
Alan Moreno <amoreno@perceptivesensors.com>
2025-02-13 14:13:40 -0700

    repositioning product detail page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -17,11 +17,11 @@ const ProductDetails = ({ product, products }) => {
   }
 
   return (
-    <div>
+    <div className="product-detail-main">
       <div className="product-detail-container">
-        <div>
-          <div className="image-container">
-            <img className="product-detail-image" src={urlFor(image && image[0])} alt="" />
+        <div className="image-container-main">
+          <div className="product-detail-image">
+            <img src={urlFor(image && image[0])} alt="" />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
@@ -33,39 +33,42 @@ const ProductDetails = ({ product, products }) => {
         </div>
 
         <div className="product-detail-desc">
-          <h1>{name}</h1>
-          <div className="reviews">
-            <div>
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiOutlineStar />
+          <div className="product-detail-desc-box">
+            <h1>{name}</h1>
+            <div className="reviews">
+              <div>
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiOutlineStar />
+              </div>
+              <p>
+                (20)
+              </p>
             </div>
-            <p>
-              (20)
-            </p>
-          </div>
-          <h4>Details:</h4>
-          <p>{details}</p>
-          <p className="price">${price}</p>
-          <div className="quantity">
-            <h3>Quantity:</h3>
-            <p className="quantity-desc">
-              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
-              <span className="num">{qty}</span>
-              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
-            </p>
-          </div>
-          <div className="buttons">
-            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
-              Add to Cart
-            </button>
-            <button type="button" className="buy-now" onClick={handleBuyNow}>
-              Buy Now
-            </button>
+            <h4>Details:</h4>
+            <p>{details}</p>
+            <p className="price">${price}</p>
+            <div className="quantity">
+              <h3>Quantity:</h3>
+              <p className="quantity-desc">
+                <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+                <span className="num">{qty}</span>
+                <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
+              </p>
+            </div>
+            <div className="buttons">
+              <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
+                Add to Cart
+              </button>
+              <button type="button" className="buy-now" onClick={handleBuyNow}>
+                Buy Now
+              </button>
+            </div>
           </div>
         </div>
+         
       </div>
       
       <div className="maylike-products-wrapper">
